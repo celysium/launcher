8c0d8385a4021e5a8f4921447b0324954d2cab36
nasser <hekmati.nasser@gmail.com>
2023-04-20 18:00:04 +0330

    add service provider

---
+++ src/LauncherServiceProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Celysium\Launcher;
+
+use Celysium\Launcher\Middleware\Authenticate;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider;
+
+class LauncherServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadRoutes();
+
+        $this->publishConfig();
+    }
+
+    public function register()
+    {
+        $this->registerConfig();
+    }
+
+    public function loadRoutes()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+    }
+
+    public function publishConfig()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/launcher.php' => config_path('launcher.php'),
+        ], 'launcher-config');
+    }
+
+
+    public function registerConfig()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/launcher.php', 'launcher'
+        );
+    }
+
+    /**
+     * @throws BindingResolutionException
+     */
+    public function registerMiddlewares()
+    {
+        $router = $this->app->make(Router::class);
+        $router->pushMiddlewareToGroup('api', Authenticate::class);
+        $router->aliasMiddleware('auth:launcher', Authenticate::class); }
+}
